5ff3daf42aaa6d56c2d2cedc64bb30a5d7b2ebf2
Rafal Kos <rafal.k@flexishore.com>
2018-03-08 08:30:07 +0100

    fix issue with error_log column. Refactor : class name change, added some logs

--- plugins/extra/itemprocessors/algolia/algoliaitemprocessor.php
+++ plugins/extra/itemprocessors/algolia/algoliaitemprocessor.php
@@ -6,7 +6,7 @@
  *
  * This processor add update products to algolia queue
  */
-class AlgoliaQueueImported extends Magmi_ItemProcessor
+class AlgoliaItemProcessor extends Magmi_ItemProcessor
 {
     public function getPluginInfo()
     {
@@ -20,16 +20,26 @@ class AlgoliaQueueImported extends Magmi_ItemProcessor
     public function processItemAfterImport(&$item, $params = null)
     {
         if (count($item) > 0) {
-            $pid=$params["product_id"];
+            try {
+                $pid=$params["product_id"];
 
-            $sql = "
-                INSERT INTO algoliasearch_queue (created, class, method, data, max_retries, retries, data_size)
-                VALUES (NOW(), 'algoliasearch/observer', 'rebuildProductIndex', ?, 3, 0, 1)
+                $sql = "
+                INSERT INTO algoliasearch_queue (created, class, method, data, max_retries, retries, error_log, data_size)
+                VALUES (NOW(), 'algoliasearch/observer', 'rebuildProductIndex', ?, 3, 0, '', 1)
             ";
-            $data = array('store_id' => 1, 'product_ids' => array($pid));
-            $this->insert($sql, json_encode($data));
+                $data = array('store_id' => 1, 'product_ids' => array($pid));
+                $this->insert($sql, json_encode($data));
+            } catch (Exception $ex) {
+                $this->log($ex->getMessage(), "error");
+            }
         }
 
+        return true;
+    }
+
+    public function afterImport()
+    {
+        $this->log("Updated products added to algolia queue.", "info");
         return true;
     }
 }
